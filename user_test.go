e02f95644776400a9eba54e80b1e4dfd1c82887b
Mert Acel <mertacel@gmail.com>
2020-07-21 19:53:00 +0300

    OK : get users

--- user_test.go
+++ user_test.go
@@ -14,11 +14,34 @@ func TestGetUsers(t *testing.T) {
 	returnValue, err = getClient().GetUser(CreateUserFilter().
 		SetEmail("mert@monoji.io").
 		SetBlocked(true))
-	ReturnAndError(t, tst, returnValue, err)*/
+	ReturnAndError(t, tst, returnValue, err)
 
 	returnValue, err := getClient().GetUser(CreateUserFilter().SetID("cf99e4d8-bc64-4a5c-80a4-dd1e25e2018d"))
 	ReturnAndError(t, tst, returnValue, err)
 
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetName("MERT"))
+	ReturnAndError(t, tst, returnValue, err)
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetPhone("123"))
+	ReturnAndError(t, tst, returnValue, err)
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetPrimaryWalletId("dc80f0c2-3360-46b4-bde0-08ccf06d2878"))
+	ReturnAndError(t, tst, returnValue, err)
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetPrimaryWalletId("dc80f0c2-3360-46b4-bde0-08ccf06d2878"))
+	ReturnAndError(t, tst, returnValue, err)
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetReferralCode("RV2EXGMMVXG"))
+	ReturnAndError(t, tst, returnValue, err)
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetReferralUserID(""))
+	ReturnAndError(t, tst, returnValue, err)
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetRole("owner"))
+	ReturnAndError(t, tst, returnValue, err)*/
+
+	returnValue, err := getClient().GetUser(CreateUserFilter().SetVerified(true))
+	ReturnAndError(t, tst, returnValue, err)
 }
 
 func TestGetUserWithID(t *testing.T) {
--- userfilter.go
+++ userfilter.go
@@ -31,3 +31,70 @@ func (u *userFilter) SetID(id ...string) *userFilter {
 	(*u)["filter[id]"] = "in:" + strings.Join(id, ",")
 	return u
 }
+
+func (u *userFilter) SetName(name string) *userFilter {
+	(*u)["filter[name]"] = name
+	return u
+}
+
+func (u *userFilter) SetPhone(phone string) *userFilter {
+	(*u)["filter[phone]"] = phone
+	return u
+}
+
+func (u *userFilter) SetPrimaryWalletId(primaryWalletId string) *userFilter {
+	(*u)["filter[primaryWalletId]"] = primaryWalletId
+	return u
+}
+
+func (u *userFilter) SetReferralCode(referralCode string) *userFilter {
+	(*u)["filter[referralCode]"] = referralCode
+	return u
+}
+
+func (u *userFilter) SetReferralUserID(referralUserId string) *userFilter {
+	(*u)["filter[referralUserId]"] = referralUserId
+	return u
+}
+
+func (u *userFilter) SetRole(role string) *userFilter {
+	(*u)["filter[role]"] = role
+	return u
+}
+
+func (u *userFilter) SetVerified(verified bool) *userFilter {
+	(*u)["filter[verified]"] = strconv.FormatBool(verified)
+	return u
+}
+
+func (u *userFilter) SetSortWithCreatedAt() *userFilter {
+	(*u)["sort"] = "createdAt"
+	return u
+}
+
+func (u *userFilter) SetSortWithCreatedAtReverse() *userFilter {
+	(*u)["sort"] = "-createdAt"
+	return u
+}
+
+func (u *userFilter) SetSortWithUpdatedAt() *userFilter {
+	(*u)["sort"] = "updatedAt"
+	return u
+}
+
+func (u *userFilter) SetSortWithUpdatedAtReverse() *userFilter {
+	(*u)["sort"] = "-updatedAt"
+	return u
+}
+
+// Defines the number of results per page. Default = 30.
+func (u *userFilter) SetPageSize(size int) *userFilter {
+	(*u)["page[size]"] = strconv.Itoa(size)
+	return u
+}
+
+// Defines the number of the page to retrieve. Default = 1
+func (u *userFilter) SetPageNumber(number int) *userFilter {
+	(*u)["page[number]"] = strconv.Itoa(number)
+	return u
+}
